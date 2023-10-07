ccc252e382a78d47ba6226f45ea4e2b42ba12236
peron <peron_ap@icloud.com>
2023-10-07 19:45:48 +0100

    feat: use deno-native implementation of parser

--- src/deps.ts
+++
@@ -1,3 +0,0 @@
-import PublicGoogleSheetsParser from "https://esm.sh/public-google-sheets-parser@1.3.2";
-
-export { PublicGoogleSheetsParser };
--- src/handler.ts
+++ src/handler.ts
@@ -1,22 +1,19 @@
-import { PublicGoogleSheetsParser } from "./deps.ts";
+import { GoogleSheet } from "https://deno.land/x/google_sheets_parser@1.0.0/mod.ts";
 
 export const handler: Deno.ServeHandler = async (req) => {
   if (req.method !== "GET") {
     return new Response("Method not allowed", { status: 405 });
   }
 
-  const parser = new PublicGoogleSheetsParser();
-
   const url = new URL(req.url);
   const [spreadsheetId, sheetName] = url.pathname.split("/").filter(Boolean);
 
   try {
-    return Response.json(
-      await parser.parse(
-        spreadsheetId,
-        sheetName ? decodeURIComponent(sheetName) : undefined,
-      ),
+    const sheet = await GoogleSheet.fetch(
+      spreadsheetId,
+      { sheetName: sheetName ? decodeURIComponent(sheetName) : undefined },
     );
+    return Response.json(sheet.entries());
   } catch (error) {
     return new Response(error.message, { status: 500 });
   }
